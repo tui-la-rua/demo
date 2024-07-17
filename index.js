22714243fb0eb418678b78e6abbb2db6e2b1d1e8
tui la rua <nam11072k4c36@gmail.com>
2024-07-17 17:39:38 +0700

    demo

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('combined'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const port = 3000
+app.listen(port, ()=>{
+  console.log(`Server is running at http://localhost:${port}`)
+})
